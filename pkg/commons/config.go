9e3cdc0d1903babe080bcb51a160e0fb8ad20aa9
NV4RE <tosbodesz@gmail.com>
2019-06-12 20:25:49 +0700

    Add Some log

--- pkg/commons/config.go
+++ pkg/commons/config.go
@@ -1,8 +1,8 @@
 package commons
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -11,8 +11,7 @@ import (
 func LoadEnv() {
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-
+		logrus.Info("Not found .env file")
 	}
 }
 
@@ -36,37 +35,36 @@ func getEnv(key string, defaultVal string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
+	logrus.Info("ENV: ", key, " not found use default: ", defaultVal)
 	return defaultVal
 }
 
-func getEnvAsInt(name string, defaultVal int) int {
-	valueStr := getEnv(name, "")
+func getEnvAsInt(key string, defaultVal int) int {
+	valueStr := getEnv(key, "")
 	if value, err := strconv.Atoi(valueStr); err == nil {
 		return value
 	}
-
+	logrus.Info("ENV: ", key, " not found use default: ", defaultVal)
 	return defaultVal
 }
 
 // Helper to read an environment variable into a bool or return default value
-func getEnvAsBool(name string, defaultVal bool) bool {
-	valStr := getEnv(name, "")
+func getEnvAsBool(key string, defaultVal bool) bool {
+	valStr := getEnv(key, "")
 	if val, err := strconv.ParseBool(valStr); err == nil {
 		return val
 	}
-
+	logrus.Info("ENV: ", key, " not found use default: ", defaultVal)
 	return defaultVal
 }
 
 // Helper to read an environment variable into a string slice or return default value
-func getEnvAsSlice(name string, defaultVal []string, sep string) []string {
-	valStr := getEnv(name, "")
-
+func getEnvAsSlice(key string, defaultVal []string, sep string) []string {
+	valStr := getEnv(key, "")
 	if valStr == "" {
 		return defaultVal
 	}
-
 	val := strings.Split(valStr, sep)
-
+	logrus.Info("ENV: ", key, " not found use default: ", defaultVal)
 	return val
 }
