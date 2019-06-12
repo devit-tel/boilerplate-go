4b2cd894b18b6453d37022a8a5e88d7c4d2bcdc2
NV4RE <tosbodesz@gmail.com>
2019-06-12 19:54:54 +0700

    Add config

---
+++ pkg/config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type ServerConfig struct {
+	Port string
+}
+
+type MongoDbConfig struct {
+	Uri string
+}
+
+type Config struct {
+	Env     string
+	Server  ServerConfig
+	MongoDb MongoDbConfig
+}
+
+func LoadEnv() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+
+	}
+}
+
+func GetConfig() *Config {
+	return &Config{
+		Env: getEnv("GO_ENV", "LOCAL"),
+		Server: ServerConfig{
+			Port: getEnv("SERVER_PORT", ":8080"),
+		},
+		MongoDb: MongoDbConfig{
+			Uri: getEnv("MONGODB_URI", ""),
+		},
+	}
+}
+
+// Simple helper function to read an environment or return a default value
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
+}
+
+func getEnvAsInt(name string, defaultVal int) int {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+
+	return defaultVal
+}
+
+// Helper to read an environment variable into a bool or return default value
+func getEnvAsBool(name string, defaultVal bool) bool {
+	valStr := getEnv(name, "")
+	if val, err := strconv.ParseBool(valStr); err == nil {
+		return val
+	}
+
+	return defaultVal
+}
+
+// Helper to read an environment variable into a string slice or return default value
+func getEnvAsSlice(name string, defaultVal []string, sep string) []string {
+	valStr := getEnv(name, "")
+
+	if valStr == "" {
+		return defaultVal
+	}
+
+	val := strings.Split(valStr, sep)
+
+	return val
+}
