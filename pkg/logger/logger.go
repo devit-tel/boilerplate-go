61398f39f5766b755ccd54010fdf35bff9fdaacb
NV4RE <tosbodesz@gmail.com>
2019-06-14 14:49:25 +0700

    Remove old comment

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -10,10 +10,8 @@ func InitLog(logConfig config.LogConfig) {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
 	logrus.SetOutput(os.Stdout)
 
-	// Only log the warning severity or above.
 	level, err := logrus.ParseLevel(logConfig.Level)
 	if err == nil {
 		logrus.SetLevel(level)
