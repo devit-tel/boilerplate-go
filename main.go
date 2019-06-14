bd32c2f9a00aad1a45275e4a6987acff3ecb5e9f
NV4RE <tosbodesz@gmail.com>
2019-06-14 14:56:12 +0700

    Handel errors

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-    "github.com/NV4RE/boilerplate-go/pkg/logger"
-    "github.com/NV4RE/boilerplate-go/pkg/http/server"
-    "github.com/NV4RE/boilerplate-go/pkg/config"
+	"github.com/NV4RE/boilerplate-go/pkg/config"
+	"github.com/NV4RE/boilerplate-go/pkg/http/server"
+	"github.com/NV4RE/boilerplate-go/pkg/logger"
 )
 
 func init() {
 }
 
 func main() {
-    cfg := config.GetConfig()
-    logger.InitLog(cfg.Log)
-	server.CreateServer(cfg.Server.Addr)
+	cfg := config.GetConfig()
+	logger.InitLog(cfg.Log)
+	sv := server.CreateServer(cfg.Server.Addr)
+	sv.Listen()
 }
